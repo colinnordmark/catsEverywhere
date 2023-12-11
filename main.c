5fe6303288a3af86725af826e0434ef1e3a65f34
colinnordmark <nordmarkcolin@gmail.com>
2023-12-11 18:45:48 +0100

    test: Testing array behaviour in C

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
+int8_t numbers[] = {
+    9,2,3,4,5
+};
+
 int main() {
-    printf("Hello, World!\n");
+    printf("Hello, World! the number is: %d \n", numbers[0]);
     return 0;
 }
